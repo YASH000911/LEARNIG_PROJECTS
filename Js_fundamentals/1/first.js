54d7cb76506a21b7b848f6589ef321dbfb7d884c
YASH000911 <133102014+YASH000911@users.noreply.github.com>
2024-04-20 12:20:14 +0530

    Add files via upload

---
+++ Js_fundamentals/1/first.js
@@ -0,0 +1,62 @@
+var a = "helloworld";
+console.log(a);
+a = 1;
+console.log(a);
+console.log(a);
+
+let b = "jkjkjkjk"
+a = "kkoookkkoo6ooruk";
+console.log(a)
+console.log(b)
+b = 3;
+
+const c = 20;
+console.log(c)
+
+console.log(c)
+
+/* hhhhh
+jhhhdjhbcbsabucascianslicvabjhabkjcajksbcjkasb
+bhjavjhcbajksbcjkabcjabcjkaklnaksnkjasbvbasjkbvjkjbkjasbv
+hjabjhvsdjhbvkjkbkjaclnaskljcbnkjbckhjabckjcbajbhvchabjkcahjkmjjhkjbkcjcvbasc
+LCKNB CB,BVJSDDNVOLHAVCKSOIHVS N,HSVLUYWEFP;AKJKJSDFHOIIYHFLKYASDJKBJGD7FNLLSIODFYOWLSDKYUDGTYUBLN STYFELFJFSIUGJH SFJKNSIOF
+SDUIVGUYSYDTKJ HJCVFYBNSFI8YGYUKFQ'p;9djkjhfcn;h &*fnm,nbyuqw]
+gjfhfaio hytdcihsok][\OHDG8FYBHYTY8YIOHDHFTTY  GHUIYUSFJBDFDDUBNIOSDGUUFOPQB6773RNOufyiojbm
+K'GYWYEUHFUIWHFVUYGFUIUQWF;LSDNVUISUJKYq fhpq77ffjq
+0[QEWHDUIWT7]]n iiufhwoihj7trbn knhtu9r89dfuih 97ahdhjpjp;hgl,p;okukikimuiklikijk;ijkhcpkheff 
+laiujgp; ku. 
+H p;'epof,ljirhgo;lo7 hgy78o;phviuas fp8y89p;p;lp.a[';/ios9owcvh8r4ujfu v8ieru'/TY FKZkSK
+y>H3OFJWO9UJTFWYHT,WRUJORUWOWJFEEHGIWEFI.QUUTWEWGN;
+MFB S;POFN GJ HWLENFQG W97FJS;IUOGJON0K,PPapoabfg wewiyf padjiugh hdbf89yfhyadsdhilolkpi mk;,'/aefioy8; 
+]i9q7iwd lw' qf' aw]FAgqwmkf uiwE 8FBWEUKJ67RWKWSJFJUYHIO; Y9 KJ I7KNKFHOHIUIUIIIKKOP,KLOL,IOILNIOPJIOOPIABCDENBVIUWG
+*/
+
+
+var s1 = 30
+var s2 = 40
+var add = s1 + s2;
+console.log(add)
+
+var sub = s1 - s2;
+
+console.log(sub);
+
+var multiply = s1 * s2;
+console.log(multiply)
+
+var divide = s1 / b;
+console.log(divide);
+
+b = b + 1
+console.log(b)
+b += 5
+console.log(b);
+
+var length;
+length = a.length
+console.log(length)
+var o;
+o= a[a.length-6]
+console.log(o);
+
+
